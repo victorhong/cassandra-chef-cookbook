7b009d456efe01225e72a7b4d34c96c96bbd83d4
Victor Hong <victor.m.hong@gmail.com>
2015-10-02 22:56:55 -0400

    Update tarball.rb

--- recipes/tarball.rb
+++ recipes/tarball.rb
@@ -98,7 +98,7 @@ directories = [node['cassandra']['log_dir'],
                node['cassandra']['root_dir'],
                node['cassandra']['conf_dir']
               ]
-directories += node['cassandra']['data_dir'] # this is an array now
+directories = directories + node['cassandra']['data_dir'] # this is an array now
 directories.each do |dir|
   directory dir do
     owner node['cassandra']['user']
